e6cf7b8f249eb69b6479fc326684e232e76d4704
SwampyFinance <105434953+SwampyFinance@users.noreply.github.com>
2022-05-15 02:21:15 -0400

    Update formatBalance.js

--- src/utils/formatBalance.js
+++ src/utils/formatBalance.js
@@ -2,17 +2,17 @@ import BigNumber from "bignumber.js";
 
 export const getBalanceNumber = (balance, decimals = 18) => {
   const displayBalance = new BigNumber(balance).dividedBy(
-    new BigNumber(10).pow(decimals)
+    new BigNumber(3).pow(decimals)
   );
   return displayBalance.toNumber();
 };
 
 export const getBeans = (balance, decimals = 9) => {
-  return balance.dividedBy(new BigNumber(10).pow(decimals)).toFixed(7);
+  return balance.dividedBy(new BigNumber(10).pow(decimals)).toFixed(3);
 };
 
 export const getFullDisplayBalance = (balance, decimals = 18) => {
-  return balance.dividedBy(new BigNumber(10).pow(decimals)).toFixed();
+  return balance.dividedBy(new BigNumber(10).pow(decimals)).toFixed(3);
 };
 
 export const getFullDisplayBalanceFixed = (
